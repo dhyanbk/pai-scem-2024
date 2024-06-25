53b6a331ca21f25408fe65f6a5bac7706d2b4914
Dhyan BK <155100153+dhyanbk@users.noreply.github.com>
2024-06-25 15:17:09 +0530

    Update set1.py

--- set1.py
+++ set1.py
@@ -1,5 +1,5 @@
-set1 = set(input("Enter elements of set 1 separated by spaces: ").split())
-set2 = set(input("Enter elements of set 2 separated by spaces: ").split())
+set1 = set(input("Enter elements of set 1 : ").split())
+set2 = set(input("Enter elements of set 2 : ").split())
 
 print("Union:", set1.union(set2))
 print("Intersection:", set1.intersection(set2))
